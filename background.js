2969fe72f4eafb17ce9c885c90e90a9dab7139e3
Frostyanimations126 <39423395+Frostyanimations126@users.noreply.github.com>
2021-02-08 14:23:39 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,14 +1,3 @@
 var browser = chrome;
-function set(vari,value){browser.storage.sync.set({vari:value})}
+function set(vari,value){return browser.storage.sync.set({vari:value})}
 function get(vari){return browser.storage.sync.get([vari])}
-chrome.browserAction.onClicked.addListener(function(tab) {
-var d = tab.url;
-  window.open("pop.html", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=250,height=100")
-});
-function colorfy(color){
-
-
-}
-function icon(name){
-chrome.browserAction.setIcon({path: "icons/"+name+".png"});
-}
