17b5a91e00b07e8a062ae4d6ca351729ec0e2884
Frostyanimations126 <39423395+Frostyanimations126@users.noreply.github.com>
2021-02-08 14:46:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,24 @@
-
+// Browser/Platform detecting configeration, this should always be loaded first.
+// If you want support for a specfic browser, submit an issue with the "Browser" label
+function detect(){
+// CHROME
+  if (navigator.userAgent.indexOf("Chrome") != -1 && !(navigator.userAgent.indexOf("Edge") != -1 ||navigator.userAgent.indexOf("Edg") != -1 )) {
+    return [chrome, "Chrome"]
+  }
+  // FIREFOX
+  else if (navigator.userAgent.indexOf("Firefox") != -1 ) {
+    return [browser, "Firefox"]
+  }
+  // EDGE
+  else if (navigator.userAgent.indexOf("Edge") != -1 ||navigator.userAgent.indexOf("Edg") != -1 ) {
+    return [chrome, "Edge"]
+  }
+  // OTHERS
+  else {
+    return [null, "Unknown"]
+  }
+};
+var config = {
+  browser:detect()[1],
+  browserName:detect()[2]
+}
